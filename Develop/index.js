b820ec2ce1635114b96e0df6f98e819d5c6b0f89
christopher gonzalez <christopher.gonzalez98@gmail.com>
2023-02-28 17:43:56 -0600

    created funtion for readme file and to init the prompt

--- Develop/index.js
+++ Develop/index.js
@@ -55,10 +55,23 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName,data,(err) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log("Your Readme.md file has been created")
+        }
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((userInput) => {
+        writeToFile('Readme.md', generateMarkdown(userInput));
+    });
+}
 
 // Function call to initialize app
 init();
