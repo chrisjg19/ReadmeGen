b7a442c063339425e80e197dd2d65c08175a8f07
christopher gonzalez <christopher.gonzalez98@gmail.com>
2023-02-28 18:49:07 -0600

    updating code for help with askbcs

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,34 +1,44 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-    if (license === 'Apache') {
-        return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]'
-    } else if (license === 'GNU'){
-        return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]'
-    } else if (license === 'MIT'){
-        return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]'
-    } else if (license === 'ISC'){
-        return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]'
-    } else {
-        return ''
+    if (license !== 'None') {
+        return `![Github license](https://img.shields.io/badge/License-${license}-blue.svg)`; 
     }
+    return '',
+    // if (license === 'Apache') {
+    //     return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)]'
+    // } else if (license === 'GNU'){
+    //     return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]'
+    // } else if (license === 'MIT'){
+    //     return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]'
+    // } else if (license === 'ISC'){
+    //     return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)]'
+    // } else {
+    //     return ''
+    // }
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-    if (license === 'Apache') {
-        return ''
-    } else if (license === 'GNU'){
-        return ''
-    } else if (license === 'MIT'){
-        return ''
-    } else if (license === 'ISC'){
-        return ''
-    } else {
-        return ''
+    if (license !== 'None') {
+        return `![Github license](https://opensource.org/licenses/${license})`; 
+    } else (license === GNU) {
+        return `![Github license](https://www.gnu.org/licenses/gpl-3.0)`
     }
 }
+//     if (license === 'Apache') {
+//         return ''
+//     } else if (license === 'GNU'){
+//         return ''
+//     } else if (license === 'MIT'){
+//         return ''
+//     } else if (license === 'ISC'){
+//         return ''
+//     } else {
+//         return ''
+//     }
+// }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
